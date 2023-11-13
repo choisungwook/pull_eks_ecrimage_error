903e4eb744ea79397b440e760add9aa59aa37c85
choisungwook <kgg1959@naver.com>
2023-11-13 21:11:41 +0900

    Kan-2 Refactor iam Auth (#2)
    * KAN-2 Docs
    
    * KAN-2 Remove IAM Role
    
    * KAN-2 Add timeout at Managed Node Group
    
    * KAN-2 docs

--- wrong_eks/main.tf
+++ wrong_eks/main.tf
@@ -78,10 +78,10 @@ module "eks" {
       capacity_type   = "SPOT",
       release_version = "1.27.1-20230513"
       disk_size       = 20
-      desired_size    = 2,
-      max_size        = 2,
-      min_size        = 2
+      desired_size    = 1,
+      max_size        = 1,
+      min_size        = 1
     }
   }
-  aws_auth_admin_roles = ["arn:aws:iam::467606240901:role/eks-admin-role"]
+  aws_auth_admin_roles = []
 }
